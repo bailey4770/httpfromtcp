dae160255849577215b5f1f2d9d7527100bb7d27
Bailey Stringer <bailey4770@protonmail.com>
2026-01-23 18:59:29 +0000

    caller can now pass handler func to server

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/bailey4770/httpfromtcp/internal/request"
+	"github.com/bailey4770/httpfromtcp/internal/response"
 	"github.com/bailey4770/httpfromtcp/internal/server"
 )
 
 const port = 42069
 
 func main() {
-	server, err := server.Serve(port)
+	server, err := server.Serve(port, testHandler)
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
@@ -23,4 +26,31 @@ func main() {
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 	<-sigChan
 	log.Println("Server gracefully stopped")
+}
+
+func testHandler(w io.Writer, req *request.Request) *server.HandlerError {
+	if req.RequestLine.RequestTarget == "/yourproblem" {
+		return &server.HandlerError{
+			StatusCode: response.StatusBadRequest,
+			ErrorBody:  "Your problem is not my problem\n",
+		}
+	}
+
+	if req.RequestLine.RequestTarget == "/myproblem" {
+		return &server.HandlerError{
+			StatusCode: response.StatusInternalServerError,
+			ErrorBody:  "Woopsie, my bad\n",
+		}
+	}
+
+	successMsg := "All good, frfr\n"
+
+	if _, err := w.Write([]byte(successMsg)); err != nil {
+		return &server.HandlerError{
+			StatusCode: response.StatusInternalServerError,
+			ErrorBody:  "Could not write to writer\n",
+		}
+	}
+
+	return nil
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -2,25 +2,38 @@
 package server
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"log"
 	"net"
 	"sync/atomic"
 
+	"github.com/bailey4770/httpfromtcp/internal/request"
 	"github.com/bailey4770/httpfromtcp/internal/response"
 )
+
+type Handler func(w io.Writer, req *request.Request) *HandlerError
 
 type Server struct {
 	listener net.Listener
 	isClosed atomic.Bool
+	handler  Handler
 }
 
-func Serve(port int) (*Server, error) {
+func Serve(port int, handler Handler) (*Server, error) {
 	listener, err := net.Listen("tcp", ":42069")
 	if err != nil {
 		return nil, err
 	}
 
-	server := &Server{listener: listener}
+	server := &Server{
+		listener: listener,
+		isClosed: atomic.Bool{},
+		handler:  handler,
+	}
+	server.isClosed.Store(false)
+
 	go server.listen()
 	return server, nil
 }
@@ -49,16 +62,69 @@ func (s *Server) listen() {
 func (s *Server) handle(conn net.Conn) {
 	defer func() { _ = conn.Close() }()
 
-	if err := response.WriteStatusLine(conn, response.StatusOK); err != nil {
-		log.Printf("Error: could not write status line: %v", err)
+	req, err := request.RequestFromReader(conn)
+	if err != nil {
+		log.Printf("Error: could not parse request: %v", err)
+		handlerErr := &HandlerError{
+			StatusCode: response.StatusBadRequest,
+			ErrorBody:  err.Error(),
+		}
+		handlerErr.write(conn)
 		return
 	}
 
-	headers := response.GetDefaultHeaders(0)
+	buf := bytes.NewBuffer([]byte{})
+	handlerErr := s.handler(buf, req)
+	if handlerErr != nil {
+		handlerErr.write(conn)
+		log.Print("Warning: handler errored. Error writen to connection")
+		return
+	}
+
+	buf.Write([]byte("\r\n"))
+
+	if err := response.WriteStatusLine(conn, response.StatusOK); err != nil {
+		log.Printf("Error: could not write response status line: %v", err)
+		handlerErr := &HandlerError{
+			StatusCode: response.StatusInternalServerError,
+			ErrorBody:  err.Error(),
+		}
+		handlerErr.write(conn)
+		return
+	}
+
+	headers := response.GetDefaultHeaders(len(buf.Bytes()))
 	if err := response.WriteHeaders(conn, headers); err != nil {
-		log.Printf("Error: could not write headers: %v", err)
+		log.Printf("Error: could not write response headers: %v", err)
+		handlerErr := &HandlerError{
+			StatusCode: response.StatusInternalServerError,
+			ErrorBody:  err.Error(),
+		}
+		handlerErr.write(conn)
+		return
+	}
+
+	if _, err := conn.Write(buf.Bytes()); err != nil {
+		log.Printf("Error: could not write response body: %v", err)
+		handlerErr := &HandlerError{
+			StatusCode: response.StatusInternalServerError,
+			ErrorBody:  err.Error(),
+		}
+		handlerErr.write(conn)
 		return
 	}
 
 	log.Print("Successfuly wrote response and closed connection")
+}
+
+type HandlerError struct {
+	StatusCode response.StatusCode
+	ErrorBody  string
+}
+
+func (h *HandlerError) write(w io.Writer) {
+	msg := fmt.Sprintf("HTTP/1.1 %d \r\n\r\n%s\r\n", h.StatusCode, h.ErrorBody)
+	if _, err := w.Write([]byte(msg)); err != nil {
+		log.Printf("Error: could not write error to connection: %v", err)
+	}
 }
